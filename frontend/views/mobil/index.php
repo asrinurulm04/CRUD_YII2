a86056f89c618a8f3478405a4da463b1e3ddd6db
Asri Nurul Ma'rifah <asrimarifah0402@gmail.com>
2022-03-15 17:53:35 +0700

    table manual

--- frontend/views/mobil/index.php
+++ frontend/views/mobil/index.php
@@ -2,35 +2,8 @@
   use yii\helpers\Html;
   use fedemotta\datatables\DataTables;
 ?>
- 
+  
 <p><?= Html::a('Create', ['create'], ['class' => 'btn btn-success']); ?></p>
-<!-- <table id="" class="table table-bordered"> 
-  <tr class="text-center">
-    <th>No Polisi</th>
-    <th>Nama</th>
-    <th>Jenis</th>
-    <th>Tahun Pembuatan</th>
-    <th>Harga Sewa</th>
-    <th>Kapasitas</th>
-    <th>Status</th>
-    <th>Action</th>
-  </tr>
-  <?php foreach($model as $field){ ?>
-  <tr class="text-center">
-    <td><?= $field->nopol; ?></td>
-    <td><?= $field->nama_mobil ?></td>
-    <td><?= $field->jenis_mobil ?></td>
-    <td><?= $field->tahun_pembuatan ?></td>
-    <td><?= $field->harga_sewa ?></td>
-    <td><?= $field->kapasitas_penumpang ?></td>
-    <td><?= $field->status_mobil ?></td>
-    <td>
-      <?= Html::a("Edit", ['mobil/edit', 'id' => $field->id_mobil],['class' => 'btn btn-warning']); ?>  
-      <?= Html::a("Delete", ['mobil/delete', 'id' => $field->id_mobil],['class' => 'btn btn-danger']); ?>
-    </td>
-  </tr>
-  <?php } ?>
-</table> -->
 
 <!-- table dengan ajax -->
 <table id="example" class="display" style="width:100%" class="table table-bordered">
